442e7b710b809b6f94c773787c6328e884b66c19
jymk <jymkyu@outlook.com>
2024-02-26 14:35:57 +0800

    修改导包lua

--- xmake.lua
+++ xmake.lua
@@ -1,20 +1,9 @@
-target('log_test')
-    set_kind('binary')
-    set_languages('cxx11')
-    
-    if is_plat('windows') then
-        -- 编译选项
-        add_cxflags('/utf-8')
-    end
-
-    add_files('test/*.cpp')
-    add_files('log.cpp')
-
 package('log')
     set_homepage('https://github.com/jymk/log.git')
     set_description('cpp log output')
 
     set_urls('https://github.com/jymk/log/archive/refs/heads/main.zip')
+    add_versions('v0.0.1', '217c58cbb30d668536faf755566b0031f7d9708f20691e8f2b27f47b42666df8')
 
     on_install(function (package) 
         io.writefile('xmake.lua', [[
@@ -31,8 +20,22 @@ package('log')
     on_test(function (package) 
         assert(package:check_cxxsnippets({test = [[
             #include <log.h>
-            void test(int argc, char** argv) {
+            void test() {
                 DEBUG("log import suc");
             }
         ]]}, {configs = {languages = "c++11"}, includes = "log.h"}))
     end)
+package_end()
+
+
+target('log_test')
+    set_kind('binary')
+    set_languages('cxx11')
+    
+    if is_plat('windows') then
+        -- 编译选项
+        add_cxflags('/utf-8')
+    end
+
+    add_files('test/*.cpp')
+    add_files('log.cpp')
